c0f03b0d29494ace7ee08aac068925e3a35fcaf1
Anna N <anna.afk@yandex.ru>
2026-01-08 03:21:56 +0300

    first step done

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from valutatrade_hub.cli.interface import InteractiveCLI
+
+
+def main():  
+    cli = InteractiveCLI()
+    cli.run()
+
+if __name__ == "__main__":
+    main()
